1666ab976f1b91484a3ce614653e89feb35df675
weu-souza <weuller1561@gmail.com>
2023-01-10 20:42:28 -0300

    first commit

---
+++ src/main/java/com/weusouza/AvaliacaoAttornatusApplication.java
@@ -0,0 +1,13 @@
+package com.weusouza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoAttornatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weusouza/AvaliacaoAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weusouza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
